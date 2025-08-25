9c946d0e770255633a5ad7ac7cb4a80a3772457d
Mithun S <mithunsrinivas28@gmail.com>
2025-08-26 00:19:28 +0530

    Update websocket-connect.py

--- aws-websocket-api-main/aws-websocket-api-main/websocket-connect.py
+++ aws-websocket-api-main/aws-websocket-api-main/websocket-connect.py
@@ -2,14 +2,16 @@ import json
 import boto3
 import os
 
-dynamodb = boto3.client('dynamodb')
+dynamodb 
+= boto3.client('dynamodb')
 
 def lambda_handler(event, context):
     connectionId = event['requestContext']['connectionId']
 
-    dynamodb.put_item(
+      dynamodb.put_item(
         TableName=os.environ['WEBSOCKET_TABLE'],
         Item={'connectionId': {'S': connectionId}}
     )
 
     return {}
+
